2246e8ee4fdc7b11781d1f172e8c97f5f7ade63f
JuanMiguel1974 <74014983+JuanMiguel1974@users.noreply.github.com>
2022-05-20 10:05:00 +0200

    ahora solo me muestra uno

--- src/controllers/estadios/lista_estadios.js
+++ src/controllers/estadios/lista_estadios.js
@@ -18,7 +18,7 @@ export default async() => {
     const estadiosElement = listaEstadiosPage.querySelector("#estadios");
     const estadios = await getEstadios();
 
-    Object.values(estadios).forEach((estadio) => {
+    /*  Object.values(estadios).forEach((estadio) => {
 
         estadiosElement.innerHTML += `
 
@@ -35,7 +35,7 @@ export default async() => {
 
         </ul>
         <div class="card-body">
-        <button type="button" class="btn btn-danger borrar" >Elimimnar</button>
+        <button type="button" ref = "${estadio.id}" class="btn btn-danger borrar" >Elimimnar</button>
             <a href="#" class="card-link">Another link</a>
         </div>`;
         console.log(estadio);
@@ -44,8 +44,8 @@ export default async() => {
         deleteButtons.forEach((button) => {
 
             button.addEventListener("click", function(event) {
-                let id = Object.entries(estadios).map(entrie => { entrie[1].id = entrie[0]; return entrie[1] });
-
+                let array = Object.entries(estadios).map(entrie => { entrie[1].id = entrie[0]; return entrie[1] });
+                let id = event.target.getAttribute("ref");
                 console.log('id =>', id);
                 fetch(`https://futbol-7727b-default-rtdb.firebaseio.com/estadios/${id}.json`, { method: 'delete', headers: { "Content-type": "application/json; charset=UTF-8" }, body: {} })
                     .then(response => response.json());
@@ -57,4 +57,43 @@ export default async() => {
 
     return listaEstadiosPage;
 
+} */
+    for (let id in estadios) {
+        let estadio = estadios[id];
+        estadio.id = id;
+
+        estadiosElement.innerHTML += `
+
+        <div class="card" style="width: 100rem;">
+        <div class="card-body">
+            <h5 class="card-title">${estadio.nombre}</h5>
+
+        </div>
+        <ul class="list-group list-group-flush">
+            <img class="card-img-top" src="${estadio.imagen}" alt="Foto estadio">
+            <li class="list-group-item">Ciudad: ${estadio.ciudad}</li>
+            <li class="list-group-item">Pais: ${estadio.pais}</li>
+            <li class="list-group-item">Aforo: ${estadio.aforo}</li>
+
+        </ul>
+        <div class="card-body">
+        <button type="button" ref="${estadio.id}" class="btn btn-danger borrar" >Elimimnar</button>
+            <a href="#" class="card-link">Another link</a>
+        </div>`;
+
+        estadiosElement
+            .querySelector(".borrar")
+            .addEventListener("click", function(event) {
+                let id = event.target.getAttribute("ref");
+
+                fetch(
+                    `https://futbol-7727b-default-rtdb.firebaseio.com/estadios/${id}.json`, {
+                        method: "delete",
+                        headers: { "Content-type": "application/json; charset=UTF-8" },
+                        body: {},
+                    }
+                ).then((response) => response.json());
+            });
+        return listaEstadiosPage;
+    }
 }
